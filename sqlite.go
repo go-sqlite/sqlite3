f18525a65817f898998c17b77e5bbb2e1923cbb4
Sebastien Binet <binet@cern.ch>
2017-12-04 12:08:32 +0100

    all: add license headers (#5)

--- sqlite.go
+++ sqlite.go
@@ -1,3 +1,6 @@
-// Package sqlite3 decodes the SQLite-3 file format
-//
+// Copyright 2017 The go-sqlite3 Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package sqlite3 decodes the SQLite-3 file format.
 package sqlite3
