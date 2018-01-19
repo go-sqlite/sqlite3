b57e325f545db253ce37589fdb7073a2f1dcd545
Sebastien Binet <binet@cern.ch>
2018-01-19 09:36:46 +0100

    sqlite3: use sub tests

--- sqlite_test.go
+++ sqlite_test.go
@@ -73,45 +73,51 @@ func TestFileOpen(t *testing.T) {
 			},
 		},
 	} {
-		f, err := Open(test.fname)
-		if err != nil {
-			t.Fatalf("%s: error: %v\n", test.fname, err)
-		}
-		defer f.Close()
-
-		if f.Version() != test.version {
-			t.Fatalf("%s: version=%d\nwant version=%d\n", test.fname, f.Version(), test.version)
-		}
-
-		if f.PageSize() != test.pagesz {
-			t.Fatalf("%s: page size = %d. want=%d", test.fname, f.PageSize(), test.pagesz)
-		}
-
-		if f.NumPage() != test.npages {
-			t.Fatalf("%s: num-pages = %d. want=%d", test.fname, f.NumPage(), test.npages)
-		}
-
-		// Check tables
-		if len(f.Tables()) != len(test.tables) {
-			t.Fatalf("%s: tables=%d, want=%d", test.fname, len(f.Tables()), len(test.tables))
-		}
-		for i := range f.Tables() {
-			ftbl := f.Tables()[i]
-			if ftbl.name != test.tables[i].name {
-				t.Errorf("table name: got=%q, want=%q", ftbl.name, test.tables[i].name)
+		t.Run(test.fname, func(t *testing.T) {
+			f, err := Open(test.fname)
+			if err != nil {
+				t.Fatalf("could not open %s: %v", test.fname, err)
 			}
-			if ftbl.pageid != test.tables[i].pageid {
-				t.Errorf("table pageid: got=%d, want=%d", ftbl.pageid, test.tables[i].pageid)
+			defer f.Close()
+
+			if f.Version() != test.version {
+				t.Errorf("%s: version=%d. want=%d", test.fname, f.Version(), test.version)
 			}
-			if len(ftbl.cols) != len(test.tables[i].cols) {
-				t.Errorf("table %s cols: got=%v, want=%v", ftbl.name, ftbl.cols, test.tables[i].cols)
+
+			if f.PageSize() != test.pagesz {
+				t.Errorf("%s: page size = %d. want=%d", test.fname, f.PageSize(), test.pagesz)
 			}
-			for j := range ftbl.cols {
-				if ftbl.cols[j].name != test.tables[i].cols[j].name {
-					t.Errorf("table %s column: got=%q, want=%q", ftbl.name, ftbl.cols[j].name, test.tables[i].cols[j].name)
+
+			if f.NumPage() != test.npages {
+				t.Errorf("%s: num-pages = %d. want=%d", test.fname, f.NumPage(), test.npages)
+			}
+
+			// Check tables
+			if len(f.Tables()) != len(test.tables) {
+				t.Errorf("%s: tables=%d, want=%d", test.fname, len(f.Tables()), len(test.tables))
+			}
+			n := len(f.Tables())
+			if n < len(test.tables) {
+				n = len(test.tables)
+			}
+			for i := 0; i < n; i++ {
+				ftbl := f.Tables()[i]
+				if ftbl.name != test.tables[i].name {
+					t.Errorf("table name: got=%q, want=%q", ftbl.name, test.tables[i].name)
+				}
+				if ftbl.pageid != test.tables[i].pageid {
+					t.Errorf("table pageid: got=%d, want=%d", ftbl.pageid, test.tables[i].pageid)
+				}
+				if len(ftbl.cols) != len(test.tables[i].cols) {
+					t.Errorf("table %s cols: got=%v, want=%v", ftbl.name, ftbl.cols, test.tables[i].cols)
+				}
+				for j := range ftbl.cols {
+					if ftbl.cols[j].name != test.tables[i].cols[j].name {
+						t.Errorf("table %s column: got=%q, want=%q", ftbl.name, ftbl.cols[j].name, test.tables[i].cols[j].name)
+					}
 				}
 			}
-		}
+		})
 	}
 
 }
